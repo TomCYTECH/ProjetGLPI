9e8982c9eafb556cfe8e1a4445dff6b04e51af8f
TomCYTECH <114582133+TomCYTECH@users.noreply.github.com>
2024-03-23 22:06:53 +0100

    role
    role

---
+++ create_link.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE LINK pau_link
+CONNECT TO your_username IDENTIFIED BY your_password
+USING 'Pau';
+
+CREATE DATABASE LINK cergy_link
+CONNECT TO user_cergy IDENTIFIED BY password_cergy
+USING 'CERGY';
