ba1b3bca240b4d514c338c5bd640979263ac0ff1
TomCYTECH <114582133+TomCYTECH@users.noreply.github.com>
2024-03-27 18:35:57 +0100

    fichier complété

---
+++ sql/views/01_create_materialise_vue.sql
@@ -0,0 +1,16 @@
+CREATE VIEW glpi_cergy_pau_view AS
+SELECT * FROM glpi_cergy
+UNION ALL
+SELECT * FROM glpi_pau;
+
+CREATE TABLE glpi_cergy_pau_materialized AS
+SELECT * FROM glpi_cergy
+UNION ALL
+SELECT * FROM glpi_pau;
+
+TRUNCATE TABLE glpi_cergy_pau_materialized;
+
+INSERT INTO glpi_cergy_pau_materialized
+SELECT * FROM glpi_cergy
+UNION ALL
+SELECT * FROM glpi_pau;
